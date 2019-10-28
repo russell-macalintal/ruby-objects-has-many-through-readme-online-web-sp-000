d5484b4647b249b126c0b1a8da2203049699b8e4
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-28 00:11:36 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,4 +16,12 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    self.meals.collect {|meal| meal.waiter}
+  end
+
 end
